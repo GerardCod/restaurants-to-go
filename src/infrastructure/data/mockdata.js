faf456001aadd7b1491857f1a0679e11f5aa73be
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-01 14:56:35 -0500

    modify: Creating restaurantsMock const

---
+++ src/infrastructure/data/mockdata.js
@@ -0,0 +1,14 @@
+export const restaurantsMock = [
+  {
+    id: 123456789,
+    name: "Cafeteria UTT"
+  },
+  {
+    id: 123654789,
+    name: "Helados San Sebastián"
+  },
+  {
+    id: 987456321,
+    name: "La Chatta"
+  }
+];

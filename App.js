60527f2ce4aa9f73210741a6b2b2f8f4a985e477
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-10-01 15:27:44 -0500

    modify: Downloading dependencies

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.sc
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
-import { useFonts as useOswald , Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MapScreen from "./src/features/map/screens/map.screen";
+import SettingsScreen from "./src/features/settings/screens/settings.screen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   let [oswaldLoaded] = useOswald({
@@ -23,7 +29,13 @@ export default function App() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
     </SafeAreaView>
